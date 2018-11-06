b68e0224279925125cb28ea3ccef16d8f0d066c0
Paul Miloro <paulatl99@gmail.com>
2018-11-05 22:21:44 -0500

    Update Solver.py
    Contains the methods for ODE integration, file I/O, and plotting. Still need to fill in the actual geodesic equations, and test the parameterization.

---
+++ Solver.py
@@ -0,0 +1,216 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+
+def dy_dx(y, x):
+    return x - y
+
+'''
+Reads ODE solution data from a file
+################################################################################
+filename -> File to read data from (file is not affected)
+################################################################################
+Return dOut -> A 2D array of the file's contents, separated by field
+'''
+def readSolData(filename):
+    #Grab and reshape the first line of data from the file so we don't need to know
+    #the actual number of fields we're taking in (WARNING: super hacky)
+    dOut = np.array([float(i) for i in open(filename, 'r').readline().rstrip().split(",")])
+
+    #1 row, n columns where n is the number of values + 1 (for the independent variable)
+    dOut = dOut.reshape((1, dOut.shape[0]))
+    #Init the line to something generic but nonempty, will be overwritten anyway
+    l = "0"
+    hasSkipped = False
+    #Properly open the file for reading this time
+    with open(filename, 'r') as file:
+        #Until we reach the last line of the file
+        while l != "":
+            #Get the line and remove whitespace/newline characters
+            l = file.readline().rstrip()
+            #Make sure we skip the first and last lines so we don't include
+            #them twice/at all, respectively
+            if l != "" and hasSkipped:
+                #Separate each field by the commas and make a new array of those
+                #values casted to floats
+                lineVals = np.array([float(i) for i in l.split(",")])
+                #Stick this new array onto the end of the output array to build
+                #up the full set of data, now separated by field
+                dOut = np.append(dOut, lineVals.reshape((1, lineVals.shape[0])), axis=0)
+            #Change this after the first loop so we don't accidentally skip
+            #everything
+            hasSkipped = True
+    #Return the full set of separated data (with statement closes file automatically)
+    return dOut
+
+'''
+Determines then writes the solution to the provided ODEs to a file
+################################################################################
+filename -> Name of file to put the data in (cleared when accessed)
+initConditions -> Initial conditions for the solver
+indVals -> Range of independent variables values we'll step across when
+    integrating the ode (e.g., timestep, in most cases)
+derivatives -> The actual ODE or system of ODE's we'll be solving
+    (defined as functions somewhere else in this file, passed "by reference")
+################################################################################
+Return True -> Returns true if writing was successful
+'''
+def writeSolData(filename, initConditions, indVals, derivatives):
+    #Find the solution with helper method
+    rVals = getSolData(initConditions, indVals, derivatives)
+    #Get the number of "timesteps" completed (rows) and the
+    #number of value fields (columns) of the 2d output array
+    steps, vals = rVals.shape
+
+    #Open the file at filename for writing (deletes its previous contents)
+    with open(filename, 'w') as file:
+        #For each "timesteps"
+        for tIndex in range(steps):
+            #write the "time" value as the first field in each row
+            file.write(str(indVals[tIndex]))
+            #For each additional value field
+            for valIndex in range(vals):
+                #comma separate them then write the value
+                file.write("," + str(rVals[tIndex, valIndex]))
+            #add a newline character to complete the line and advance the loop
+            file.write("\n")
+    #Return True if writing succeeded
+    return True
+
+'''
+Helper method that actually does the integration for writeSolData()
+(for possible expansion later, currently fairly pointless)
+################################################################################
+initConditions -> Initial conditions for the solver
+indVals -> Range of independent variables values we'll step across when
+    integrating the ode (e.g., timestep, in most cases)
+derivatives -> The actual ODE or system of ODE's we'll be solving
+    (defined as functions somewhere else in this file, passed "by reference")
+################################################################################
+Returns the odeint() method's output for the given conditions/derivatives
+'''
+def getSolData(initConditions, indVals, derivatives):
+    return odeint(derivatives, initConditions, indVals)
+
+
+'''
+Default R^N to R^2 mapping for the plotSolData() method; truncates the input
+    array to pass only the first two fields directly as the "x" and "y" of the
+    parameterization; expected to be replaced by additional methods for any
+    realistic application, but provides an example of what is necessary
+################################################################################
+arr2 -> 2D (N, V) array containing some time and/or axis data
+################################################################################
+Return outArray -> A 2D (N, 2) array that's mapped the V columns of the input
+    array to just two in the output array, in this case by simply truncation
+'''
+def defaultConversion(arr2):
+    #Grab the first row and first two columns of the input array to serve as a
+    #"seed" for the output array
+    outArray = arr2[0, 0:2]
+    #For all the other values (from the second row on)
+    for index in range(1, arr2.shape[0]):
+        #Append the same first-two-column-slice as additional rows of the
+        #output array.
+        #In an actual application, this row would be replaced by the desired
+        #coordinate conversion between the V elements and Cartesian coordinates.
+        outArray.append(arr2[index, 0:2], axis=0)
+    #Return the now (N, 2) mapped array as the output
+    return outArray
+
+'''
+Main plotting method that creates, saves, and displays plots of provided data
+***Restricted to 2D plots at the moment***
+################################################################################
+fullArray -> 2D Array of all "timesteps" and all data fields; rows = values for
+    each timestep, columns = values for each field therein
+shouldParameterize -> Should we make a parametric plot, or plot each field
+    separately?
+shouldShow -> Should we immediately display the created plots?
+shouldSave -> Should we save the created plots?
+filename -> If shouldSave is True, then the created plots will go here.
+plotTitle -> Title of the plot to be created
+timeUnits -> Units for the x or "timestep" axis if shouldParameterize is False
+labels -> List of value-field axis labels, defaults to basic alphabetical
+yUnits -> List of corresponding units for those labels, defaults to "meters"
+conversion -> If shouldParameterize is True, this is the mapping between the R^N
+    "time" and value fields and the R^2 x, y Cartesian coordinates we'll plot
+    with; like the ODE equations is a method defined somewhere else, passed
+    "by reference." Defaults to truncation of 2+N value fields, passing the
+    first two non-timestep fields as x and y directly.
+################################################################################
+Return True -> Returns true if the plotting, saving, and/or displaying succeeded
+'''
+def plotSolData(fullArray, shouldParameterize, shouldShow, shouldSave,
+ filename = "defaultFilename.png",
+ plotTitle = "Output",
+ timeUnits="seconds",
+ labels=[chr(i) for i in range(97,123,1)],
+ yUnits=[""],
+ conversion=defaultConversion):
+
+    #Grab the timesteps into their own array for convenient access later
+    tSteps = fullArray[:, 0]
+
+    #If we should make a parametric plot
+    if shouldParameterize:
+        #Get the x and y arrays using our specified conversion by passing
+        #the "coordinate" value fields into it, effectively transforming our
+        #data into a Cartesian coordinate path
+        x, y = conversion(fullArray[1:, :])
+        #Generate default unit labels for x and y if none were given
+        if yUnits[0] == "":
+            yUnits = ["meters" for i in range(2)]
+        #Title the plot
+        plt.title(plotTitle)
+        #Label the x and y axes "label_name (units)"
+        plt.xlabel(labels[0] + " (" + yUnits[0] + ")")
+        plt.ylabel(labels[1] + " (" + yUnits[1] + ")")
+        #Plot the x values versus the y values, which will implicitly be parametric
+        plt.plot(x, y)
+    #If we're not making parametric plots
+    else:
+        #Get the size of the "coordinate" fields in the input array
+        n = fullArray.shape[1] - 1
+        #Title the overall plot
+        plt.title(plotTitle)
+        #Generate default unit labels for each axis if none were given
+        if yUnits[0] == "":
+            yUnits = ["meters" for i in range(n)]
+        #For each coordinate field, we will make additional subplots which will
+        #automatically be composited into a single larger one for ease of
+        #saving and display
+        for axis in range(n):
+            #Make a new subplot in the (n, 1) grid (e.g., successive axes will
+            #move rightwards)
+            plt.subplot(n, 1, axis)
+            #Label the x-axis as time
+            plt.xlabel("Time (" + timeUnits + ")")
+            #and the y-axis as whatever the name of the coordinate is
+            plt.ylabel(labels[axis - 1] + " (" + yUnits[axis - 1] + ")")
+
+            #Then plot it with x using the timestep data and y using the
+            #coordinate data for that axis
+            plt.plot(tSteps, fullArray[:, axis])
+
+    #If we should save the plot, write the resulting plot to the given filename
+    if shouldSave:
+        plt.savefig(filename)
+    #If we should show the plot, display it
+    if shouldShow:
+        plt.show()
+
+    #Return True if plot generation was successful
+    return True
+
+
+################################################################################
+# For command-line interaction (entry into the program is below)
+################################################################################
+writeSolData("test.dat", 1.0, np.linspace(0,5,100), dy_dx)
+solData = readSolData("test.dat")
+#print(solData[:, 0])
+#showSolData(solData[:, 0], solData[:, 1])
+plotSolData(solData, False, True, True, filename = "test.png", labels=["y"])
+################################################################################
